ceba05e2605e7ea2d1f5d6721ced57670c5a25c0
Speloquin <speloqu24@gmail.com>
2020-10-11 17:51:08 -0600

    README updated, screenshots added to 'Assets' folder

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Password_Generator
+# Project Objective
+
+The main objective of this project was to create a password application that provided the user with a clean, easy interface used to generate a randomized password based off of the criteria they decide. 
+
+# Variable Declaration & Arrays
+
+Using arrays and Scoped Variables I am able to create internal list that are acceptable characters for the user. These arrays trigger bolean values and are stored in an empty array. This "passwordPool" array contains all of the conditions that were selected by the user to include in their final generated password. 
+
+The passwordPool array is then looped for a final result. 
+
+# Functions
+
+The main function that runs is the "passwordGen" function. This function runs all of the scope variables, confirms, conditions and loops needed to create a final result to the user. 
+
+# Screenshots
+
+
+
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Declaring GLOBAL Variables
@@ -16,16 +17,19 @@ function passwordGen() {
   // Prompting the user to select a number for password length 
   var userNumber = parseInt(prompt("Choose a number between 8 - 128 to generate your password!"));
 
+  // If user chooses a number outside 8 and 128, alert user to  try again. 
   if (userNumber < 8 || userNumber > 128) {
     alert("The value you entered is incorrect. Please try again.");
   }
 
-  // CONFIRM WITH USER TO INCLUDE LOWERCASE
+  // Declare password characters
   var wantLower = confirm("Would you like to include lowercase letters in your password?");
-  var wantUpper = confirm("Would you like to include Uppercase letters in your password?");
+  var wantUpper = confirm("Would you like to include uppercase letters in your password?");
   var wantNumber = confirm("Would you like to include numbers in your password?");
   var wantSpecial = confirm("Finally, would you like to include special characters in your password?");
 
+
+  // Conditions
   if (wantUpper) {
     passwordPool = passwordPool.concat(upperCase);
   }
@@ -41,17 +45,17 @@ function passwordGen() {
     passwordPool = passwordPool.concat(specialChar);
   }
 
-
+  // Error condition - If all values equal false alert user to choose a criteria. 
   if (wantUpper == false && wantLower == false && wantNumber == false && wantSpecial == false) {
     alert("Please enter a password criteria.");
     return passwordGen();
   }
 
 
-  // displays all of the arrays of chars that are true
+  // Displays array characters that are true
   console.log(passwordPool);
 
-  // This for loop is looping through the variable arrays that have been pushed to the passwordPool
+  // This for loop goes through the variable arrays that have been pushed to the passwordPool
   var result = "";
   for (var i = 0; i < userNumber; i++) {
     result = result += passwordPool[Math.floor(Math.random() * passwordPool.length)];
@@ -68,10 +72,6 @@ function writePassword() {
 
   passwordText.value = password;
 }
-
-// Assignment Code 
-// writePassword();
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
